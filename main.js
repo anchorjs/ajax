4f0e2dbc3b8b1a5a9ef1a27019deecd34cb25e21
Jared Hanson <jaredhanson@gmail.com>
2012-12-28 11:51:13 -0800

    Support headers option to request.

--- main.js
+++ main.js
@@ -4,19 +4,25 @@ define(['exports',
 function(exports, Request, uri) {
 
   function request(url, method, cb) {
+    var headers;
+    
     if (typeof url == 'object') {
       var opts = url;
       cb = method;
       method = opts.method || 'GET';
       url = uri.format(opts);
-      
-      // TODO: set headers from opts.headers
+      headers = opts.headers;
     } else if (typeof method == 'function') {
       cb = method;
       method = 'GET';
     }
     
     var req = new Request(url, method);
+    if (headers) {
+      for (var name in headers) {
+        req.setHeader(name, headers[name]);
+      }
+    }
     if (cb) req.on('response', cb);
     return req;
   }
